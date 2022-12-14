f19b7af34189752885bfcbeda8129a58d1792d63
Leo <thinhphatmai2001@gmail.com>
2022-12-14 09:02:35 +0700

    am

--- phat/Login.py
+++ phat/Login.py
@@ -10,35 +10,75 @@ class LoginFeature(unittest.TestCase):
         self.driver = webdriver.Firefox()
         self.driver.get("http://localhost/OnlinePizzaDelivery/")     
 
-    def test_login_in_python(self): 
+    def test_login_username_true_password_wrong(self): 
         if self.accessOk():            
             self.findLoginbtn().click()            
-            self.driver.find_element(By.ID, 'loginusername').send_keys("usser1")
-            self.driver.find_element(By.ID, 'loginpassword').send_keys("user1")
+            self.driver.find_element(By.ID, 'loginusername').send_keys("user1")
+            self.driver.find_element(By.ID, 'loginpassword').send_keys("u@@@@@@@@ser1")
             time.sleep(1)
             for sub in self.findsubmitlogin():
                 if sub.text == 'Submit':
                     sub.click()
                     break                    
-            # time.sleep(2)
-
-            
-            warningstring = 'Warning! Invalid Credentials' if  self.driver.find_elements(By.CSS_SELECTOR,".alert.alert-warning.alert-dismissible.fade.show") else 'true'
-            self.assertNotIn('Warning', warningstring)
+           
+            warningstring = 'Warning! Invalid Credentials' if  self.driver.find_elements(By.CSS_SELECTOR,".alert.alert-warning.alert-dismissible.fade.show") else 'false'
+            self.assertIn('Warning', warningstring)
             
             
-    def test_login_in_python2(self): 
+    def test_login_not_enter_username(self): 
         if self.accessOk():            
             self.findLoginbtn().click()            
-            self.driver.find_element(By.ID, 'loginusername').send_keys("usser1")
+            # self.driver.find_element(By.ID, 'loginusername').send_keys("u2ser1")
             self.driver.find_element(By.ID, 'loginpassword').send_keys("user1")
             time.sleep(1)
             for sub in self.findsubmitlogin():
                 if sub.text == 'Submit':
                     sub.click()
-                    break            
-            warningstring = 'Warning! Invalid Credentials' if  self.driver.find_elements(By.CSS_SELECTOR,".alert.alert-warning.alert-dismissible.fade.show") else 'true'
-            self.assertNotIn('Warning', warningstring)
+                    e = self.driver.find_element(By.ID, 'loginusername').get_attribute("required")
+                    break 
+            # print(e)
+            self.assertEqual(e,"true")
+            
+            
+    def test_login_not_enter_password(self): 
+        if self.accessOk():            
+            self.findLoginbtn().click()            
+            self.driver.find_element(By.ID, 'loginusername').send_keys("user1")
+            # self.driver.find_element(By.ID, 'loginpassword').send_keys("user1")
+            time.sleep(1)
+            for sub in self.findsubmitlogin():
+                if sub.text == 'Submit':
+                    sub.click()
+                    e = self.driver.find_element(By.ID, 'loginpassword').get_attribute("required")
+                    break 
+            # print(e)
+            self.assertEqual(e,"true")
+            
+            
+            
+    def test_login_success(self): 
+        # having username : user1 and password: user1 in database
+        if self.accessOk():            
+            self.findLoginbtn().click()            
+            self.driver.find_element(By.ID, 'loginusername').send_keys("user1")
+            self.driver.find_element(By.ID, 'loginpassword').send_keys("user1")
+            time.sleep(1)
+            for sub in self.findsubmitlogin():
+                if sub.text == 'Submit':
+                    sub.click()
+                    break 
+        succ = 'true' if  self.driver.find_elements(By.CSS_SELECTOR,".rounded-circle") else 'false'
+        self.assertEqual(succ,"true")
+            
+            
+    
+    # def test_a(self):
+    #     if self.accessOk():            
+    #         self.findLoginbtn().click()            
+    #         e = self.driver.find_element(By.ID, 'loginusername').get_attribute("required")
+    #         j = self.driver.find_element(By.ID, 'loginpassword').get_attribute("required")
+    #         print(e)
+    #         print(j)
 
     def findsubmitlogin(self):
         return self.driver.find_elements(By.CSS_SELECTOR, ".btn.btn-success")
