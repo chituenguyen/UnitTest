81cf0ba91e74f960af6bf80aaf309f5f2e016729
Leo <thinhphatmai2001@gmail.com>
2022-12-14 07:57:49 +0700

    a

--- phat/Login.py
+++ phat/Login.py
@@ -8,9 +8,9 @@ class LoginFeature(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Firefox()
-        self.driver.get("http://localhost/OnlinePizzaDelivery/")
+        self.driver.get("http://localhost/OnlinePizzaDelivery/")     
 
-    def test_login_in_python(self):    
+    def test_login_in_python(self): 
         if self.accessOk():            
             self.findLoginbtn().click()            
             self.driver.find_element(By.ID, 'loginusername').send_keys("usser1")
@@ -26,9 +26,19 @@ class LoginFeature(unittest.TestCase):
             warningstring = 'Warning! Invalid Credentials' if  self.driver.find_elements(By.CSS_SELECTOR,".alert.alert-warning.alert-dismissible.fade.show") else 'true'
             self.assertNotIn('Warning', warningstring)
             
-
-                                 
-        return False
+            
+    def test_login_in_python2(self): 
+        if self.accessOk():            
+            self.findLoginbtn().click()            
+            self.driver.find_element(By.ID, 'loginusername').send_keys("usser1")
+            self.driver.find_element(By.ID, 'loginpassword').send_keys("user1")
+            time.sleep(1)
+            for sub in self.findsubmitlogin():
+                if sub.text == 'Submit':
+                    sub.click()
+                    break            
+            warningstring = 'Warning! Invalid Credentials' if  self.driver.find_elements(By.CSS_SELECTOR,".alert.alert-warning.alert-dismissible.fade.show") else 'true'
+            self.assertNotIn('Warning', warningstring)
 
     def findsubmitlogin(self):
         return self.driver.find_elements(By.CSS_SELECTOR, ".btn.btn-success")
